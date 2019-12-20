a954bc66ea1483745229540bea4cd9cd95bf1061
ConstanceGay <constancegaycsi@gmail.com>
2019-12-20 10:28:31 +0100

    modified topo to add datacenter

--- script_docker.py
+++ script_docker.py
@@ -41,6 +41,7 @@ logging.getLogger('api.openstack.heat').setLevel(logging.DEBUG)
 logging.getLogger('api.openstack.heat.parser').setLevel(logging.DEBUG)
 logging.getLogger('api.openstack.glance').setLevel(logging.DEBUG)
 logging.getLogger('api.openstack.helper').setLevel(logging.DEBUG)
+setLogLevel('info')
 
 
 def create_topology():
@@ -58,9 +59,28 @@ def create_topology():
     rapi1.connectDatacenter(dc1)
     rapi1.start()
 
+    info('*** Adding docker containers\n')
+    GF = net.addDocker('GF', ip='10.0.0.201', dimage="constancegay/projet_sdci:zone")
+    GI = net.addDocker('GI', ip='10.0.0.202', dimage="constancegay/projet_sdci:GI")
+    srv = net.addDocker('srv', ip='10.0.0.203', dimage="constancegay/projet_sdci:server")
+
+    info('*** Adding switches\n')
+    s1 = net.addSwitch('s1')
+    s2 = net.addSwitch('s2')
+
+    info('*** Creating links\n')
+    net.addLink(srv, s1)
+    net.addLink(s1, GI)
+    net.addLink(GI, s2)
+    net.addLink(s2, GF)
+
+    info('*** Starting network\n')
     net.start()
-    net.CLI()
-    # when the user types exit in the CLI, we stop the emulator
+    info('*** Testing connectivity\n')
+    net.ping([srv, GI])
+    info('*** Running CLI\n')
+    CLI(net)
+    info('*** Stopping network')
     net.stop()
 
 
@@ -70,50 +90,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-
-
-
-
-
-
-#!/usr/bin/python
-"""
-This is the most simple example to showcase Containernet.
-"""
-from mininet.net import Containernet
-from mininet.node import Controller
-from mininet.cli import CLI
-from mininet.link import TCLink
-from mininet.log import info, setLogLevel
-setLogLevel('info')
-
-net = Containernet(controller=Controller)
-info('*** Adding controller\n')
-net.addController('c0')
-
-info('*** Adding docker containers\n')
-GF = net.addDocker('GF', ip='10.0.0.201', dimage="constancegay/projet_sdci:zone")
-GI = net.addDocker('GI', ip='10.0.0.202', dimage="constancegay/projet_sdci:GI")
-srv = net.addDocker('srv', ip='10.0.0.203', dimage="constancegay/projet_sdci:server")
-
-info('*** Adding switches\n')
-s1 = net.addSwitch('s1')
-s2 = net.addSwitch('s2')
-
-info('*** Creating links\n')
-net.addLink(srv, s1)
-net.addLink(s1, GI)
-net.addLink(GI, s2)
-net.addLink(s2, GF)
-
-info('*** Starting network\n')
-net.start()
-info('*** Testing connectivity\n')
-net.ping([srv, GI])
-info('*** Running CLI\n')
-CLI(net)
-info('*** Stopping network')
-net.stop()
-
