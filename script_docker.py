9af755094a5656a3bfea996276d23c062e600379
ConstanceGay <constancegaycsi@gmail.com>
2019-12-20 10:01:49 +0100

    modif containernet

--- script_docker.py
+++ script_docker.py
@@ -14,19 +14,19 @@ info('*** Adding controller\n')
 net.addController('c0')
 
 info('*** Adding docker containers\n')
-#GF = net.addDocker('GF', ip='10.0.0.201', dimage="constancegay/projet_sdci:zone")
+GF = net.addDocker('GF', ip='10.0.0.201', dimage="constancegay/projet_sdci:zone")
 GI = net.addDocker('GI', ip='10.0.0.202', dimage="constancegay/projet_sdci:GI")
 srv = net.addDocker('srv', ip='10.0.0.203', dimage="constancegay/projet_sdci:server")
 
 info('*** Adding switches\n')
 s1 = net.addSwitch('s1')
-#s2 = net.addSwitch('s2')
+s2 = net.addSwitch('s2')
 
 info('*** Creating links\n')
 net.addLink(srv, s1)
 net.addLink(s1, GI)
-#net.addLink(GI, s2)
-#net.addLink(s2, GF)
+net.addLink(GI, s2)
+net.addLink(s2, GF)
 
 info('*** Starting network\n')
 net.start()
