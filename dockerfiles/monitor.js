f9fe1b23692676cb90f0a0fdb29fc7d43c7888c6
PierreBinet <pbinet@etud.insa-toulouse.fr>
2020-01-14 22:03:36 +0100

    fixed mistake

--- dockerfiles/monitor.js
+++ dockerfiles/monitor.js
@@ -62,8 +62,7 @@ app.get('/monitor/', function(req, res) {
     latSRV = getLatency(SRV_ENDPOINT.IP, SRV_ENDPOINT.PORT);
     latGI = getLatency(GI_ENDPOINT.IP, GI_ENDPOINT.PORT);
     res.send({CPUSRV : cpuSRV}, {CPUGI : cpuGI}, {LATSRV : latSRV}, {LATGI : latGI});
-    res.end();
-}
+});
 
 
 
