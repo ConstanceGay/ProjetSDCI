1737411087a3417150bb05b7b7606c59ca0c0005
PierreBinet <pbinet@etud.insa-toulouse.fr>
2020-01-17 20:00:43 +0100

    added cpulat for the gateway

--- dockerfiles/scriptJS/gateway.js
+++ dockerfiles/scriptJS/gateway.js
@@ -5,12 +5,15 @@ app.use(express.json()) // for parsing application/json
 var request = require('request');
 const si = require('systeminformation');
 var argv = require('yargs').argv;
+// desired arguments in the following order
 // --local_ip
 // --local_port
 // --local_name
 // --remote_ip
 // --remote_port
 // --remote_name
+
+
 
 var LOCAL_ENDPOINT = {IP : argv.local_ip, PORT : argv.local_port, NAME : argv.local_name};
 var REMOTE_ENDPOINT = {IP : argv.remote_ip, PORT : argv.remote_port, NAME : argv.remote_name};
@@ -20,6 +23,7 @@ const E_CREATED         = 201;
 const E_FORBIDDEN       = 403;
 const E_NOT_FOUND       = 404;
 const E_ALREADY_EXIST   = 500;
+
 
 
 var db = {
@@ -58,6 +62,62 @@ function register() {
     );
 }
 
+
+
+///////////////////////////
+//CPU LOAD DETECTION PART//
+///////////////////////////
+
+var os = require("os");
+
+// found on https://gist.github.com/bag-man/5570809
+function cpuAverage() {
+  //Initialise sum of idle and time of cores and fetch CPU info
+  var totalIdle = 0, totalTick = 0;
+  var cpus = os.cpus();
+
+  //Loop through CPU cores
+  for(var i = 0, len = cpus.length; i < len; i++) {
+
+    //Select CPU core
+    var cpu = cpus[i];
+
+    //Total up the time in the cores tick
+    for(type in cpu.times) {
+      totalTick += cpu.times[type];
+   }     
+
+    //Total up the idle time of the core
+    totalIdle += cpu.times.idle;
+  }
+
+  //Return the average Idle and Tick times
+  return {idle: totalIdle / cpus.length,  total: totalTick / cpus.length};
+}
+
+// found on https://gist.github.com/bag-man/5570809
+function CPULoad(avgTime, callback) {
+  this.samples = [];
+  this.samples[1] = cpuAverage();
+  this.refresh = setInterval(() => {
+    this.samples[0] = this.samples[1];
+    this.samples[1] = cpuAverage();
+    var totalDiff = this.samples[1].total - this.samples[0].total;
+    var idleDiff = this.samples[1].idle - this.samples[0].idle;
+    callback(1 - idleDiff / totalDiff);
+  }, avgTime);
+}
+
+
+
+app.get('/cpulat', function(req, res) {
+    console.log(req.body);
+    // load average for the past 1000 milliseconds
+    var cpuDataItem = CPULoad(1000, (load) => console.log((100*load).toFixed(1)));
+
+    res.write(cpuDataItem.toString());
+    res.end();
+});
 
 app.post('/gateways/register', function(req, res) {
     console.log(req.body);
