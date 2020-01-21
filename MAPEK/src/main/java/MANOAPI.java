6a4889e6d5bde83cc7e69817bd63671b2a06c561
ConstanceGay <constancegaycsi@gmail.com>
2020-01-21 22:40:03 +0100

    Fixed request mistakes

--- MAPEK/src/main/java/MANOAPI.java
+++ MAPEK/src/main/java/MANOAPI.java
@@ -56,7 +56,6 @@ class MANOAPI {
                     "http://127.0.0.1:5001/restapi/compute/dc1/lb -H 'Content-Type: application/json' -d " +
                     "'{\"image\":\"constancegay/projet_sdci:lb\", \"network\":\"(id=lb-eth0,ip="
                     + ips.get(0)+"/24)\"}'");
-            
           //build response into a string
             StringBuilder output = new StringBuilder();
             BufferedReader reader = new BufferedReader(new InputStreamReader(process2.getInputStream()));
